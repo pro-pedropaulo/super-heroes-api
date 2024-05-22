a7d98922d7bba4f4075fb76491fd8f1489225b32
Pedro Silva <pedro.silva@b2ml.com.br>
2024-05-22 00:26:18 -0300

    feat: Add BlacklistService for managing blacklisted tokens

---
+++ src/authentication/services/blacklistService.ts
@@ -0,0 +1,15 @@
+import { injectable } from 'tsyringe';
+
+import { redisConnect } from '../../shared/infra/redis/redisConnect';
+@injectable()
+export class BlacklistService {
+  async addToBlacklist(token: string) {
+    const client = await redisConnect();
+    await client.set(token, 'blacklisted');
+  }
+
+  async isTokenBlacklisted(token: string) {
+    const client = await redisConnect();
+    return await client.get(token);
+  }
+}
