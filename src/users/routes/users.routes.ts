c6e431aafd8fdb523be135dc97843a553e85d15c
Pedro Silva <pedro.silva@b2ml.com.br>
2024-05-22 00:21:03 -0300

    feat: Add routes for creating, finding, updating, and deleting users

---
+++ src/users/routes/users.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+
+import { UsersController } from '../controller/UsersController';
+import { upload } from '../../shared/container/providers/multer/multer';
+import { isAuth } from '../../shared/infra/http/middlewares/IsAuth';
+
+const UsersRouter = Router();
+const usersController = new UsersController();
+
+UsersRouter.post('/', upload.single('profilePhoto'), (req, res) =>
+  usersController.createUser(req, res),
+);
+
+UsersRouter.get('/:id', isAuth, (req, res) =>
+  usersController.findById(req, res),
+);
+
+UsersRouter.get('/', isAuth, (req, res) => usersController.getAll(req, res));
+
+UsersRouter.put('/:id', isAuth, upload.single('profilePhoto'), (req, res) =>
+  usersController.updateUser(req, res),
+);
+
+UsersRouter.delete('/:id', isAuth, (req, res) =>
+  usersController.delete(req, res),
+);
+
+export { UsersRouter };
